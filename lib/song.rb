733e829e5f9cf36381baf15d86a3e6fc7e2f48f7
jessi ortega <ortegabx05@gmail.com>
2019-07-31 15:38:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,4 +7,24 @@ class Song
   @@genres = []
   @@artists = []
   
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
+  end
+
+  def self.count
+    @@song_count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
 end
